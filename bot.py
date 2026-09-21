c6ba643dd5412343f9050308bba168a6c0a7e193
matolimp4-debug <matolimp4@gmail.com>
2026-09-22 02:55:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from aiogram.types import (
 )
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN_HERE"  # <-- ВСТАВЬ СЮДА СВОЙ ТОКЕН
+BOT_TOKEN = "8839579656:AAFMvxw5FGNgkBI_vGQjCkG5engkrwHELOU"  
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
